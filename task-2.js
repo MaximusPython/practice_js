cdf9d5f79c2d0a2638f344c57ef00944b88fa96b
Maximus <85298325+MaximusPython@users.noreply.github.com>
2022-11-21 13:07:15 +0300

    change

--- task-2.js
+++ task-2.js
@@ -13,36 +13,6 @@ function findShort(s){
 
 
 
-   // Задача
-
-// Кодирование в функции cutIt, функция принимает 1 параметр: обр. arr — массив строк.
-
-// Первая миссия: Обходя обр, найти кратчайшую длину строки.
-
-// Вторая миссия: снова пройти через arr, перехватить все строки до кратчайшей длины строки (начиная с index0). 
-// вы можете использовать один из slice() substring() или substr() сделать это. вернуть результат после завершения работы.
-
-function cutIt(arr){
-  k = []
-  f = []
-  m = 0
-  for (i of arr) {
-      k.push(i.length)         
-      let min = Math.min.apply(null, k)
-      m = min
-      }
-      
-  for (i of arr) {
-      f.push(i.slice(0, m))
-
-  }   
-  
-       
-  
-  return f
-}
-
-console.log(cutIt(["codewars","javascript","java"]))
 
 
  
